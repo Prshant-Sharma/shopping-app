52222f25166df00eb97cf427bf367b9461832be6
Prshant-Sharma <prshantsharma001@gmail.com>
2021-04-16 18:29:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login';
+import ProductList from './Components/ProductList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Login}></Route>
+          <Route exact path='/product-list' component={ProductList}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/ProductList.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+
+function ProductList() {
+    const [products, setProducts] = useState([]);
+
+    return (
+        <div>
+            <h2>Products</h2>
+        </div>
+    )
+}
+
+export default ProductList
